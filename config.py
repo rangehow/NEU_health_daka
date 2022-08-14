2e81c675dfbcb7892ad2cac0b153ee2b2c56a541
Rangehow <88258534+rangehow@users.noreply.github.com>
2022-08-14 10:21:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,26 @@
 ### 基础部分
 # 填写你的学号
-stutendID = ''
 
-# 填写你的密码
-passward = ''
+stutendID = os.environ['STUTENDID']
+passward = os.environ['PASSWARD']
+mailPass= os.environ['MAILPASS']
+receiver = os.environ['RECEIVER']
 
 
-### 通知部分,打卡失败进行邮件通知
-# 当相应渠道设置后，将自动推送通知
+
 
 # 是否仅当打卡失败时才通知，False则打卡成功失败皆通知
 sendMsgOnlyError = True
 
-## 邮件通知
-# 设置自己接收打卡信息通知的邮箱(XXXX@qq.com)
-receiver = ''
+
 
 # 发送的邮箱，这里可以设置成与接收邮箱一致（即自己发给自己），或者也可以用其他的
 sender = receiver
 
-# 填写发送邮箱SMTP授权码/密码
-mailPass = ''
+
 
 # 填写发送邮箱的SMTP服务器地址(smtp.qq.com)
-mailHost = ''
+mailHost = 'smtp.qq.com'
 
 ## ServerChan通知
 # Server酱的SCKEY,可以在Server酱的网站注册获取
